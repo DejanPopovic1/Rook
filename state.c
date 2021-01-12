711e89a4433187579f54662431d707050e3e90d8
Dejan Popovic <DejanP@letsema.co.za>
2021-01-12 21:53:39 +0200

    Defined player colour position bitboards

--- state.c
+++ state.c
@@ -1,1 +1,16 @@
+#include <stdint.h>
 
+typedef uint64_t T_bitboard;
+
+T_bitboard wPawn;
+T_bitboard wBishop;
+T_bitboard wKnight;
+T_bitboard wRook;
+T_bitboard wQueen;
+T_bitboard wKing;
+T_bitboard bPawn;
+T_bitboard bBishop;
+T_bitboard bKnight;
+T_bitboard bRook;
+T_bitboard bQueen;
+T_bitboard bKing;
