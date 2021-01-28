7a4893248483ea41e449fdac289c52e3609fdda4
Dejan Popovic <DejanP@letsema.co.za>
2021-01-28 22:21:49 +0200

    added promote Rook

--- moveGenerator.c
+++ moveGenerator.c
@@ -93,13 +93,13 @@ void genWPawnSuccStates(T_boardStates *dst, const T_boardState *b, int n){
         clearBit(b->wPawn, n);
         addState(dst, &cpy);
     }
-//        //ROOK
-//    if(isRankSeven(n) && isUpEmpty(b, n)){
-//        T_boardState *cpy = b;
-//        setBit(b->wRook, n + 8);
-//        clearBit(b->wPawn, n);
-//        addState(dst, cpy);
-//    }
+        //ROOK
+    if(isRankSeven(n) && isUpEmpty(b, n)){
+        T_boardState cpy = *b;
+        setBit(b->wRook, n + 8);
+        clearBit(b->wPawn, n);
+        addState(dst, &cpy);
+    }
 //        //QUEEN
 //    if(isRankSeven(n) && isUpEmpty(b, n)){
 //        T_boardState *cpy = b;
