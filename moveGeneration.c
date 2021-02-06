2516d7e460d6fd495c6f525747c95f27f3b8f637
Dejan Popovic <dejan.dp.popovic@gmail.com>
2021-02-06 17:49:19 +0200

    genBQueen moveGeneration

--- moveGeneration.c
+++ moveGeneration.c
@@ -392,7 +392,6 @@ void genRaySuccStates(T_boardStates *dst, const T_boardState *b, int n, const T_
             genDirStates(dst, b, n, rays, northWest, blackBishop);
             break;
         case blackRook:
-            printf("TESTING\n");
             genDirStates(dst, b, n, rays, north, blackRook);
             genDirStates(dst, b, n, rays, east, blackRook);
             genDirStates(dst, b, n, rays, south, blackRook);
@@ -427,8 +426,8 @@ void genSuccStates(T_boardStates *dst, const T_boardState *b){
             //genPiecesSuccStates(dst, b, rays, blackPawn);
             //genPiecesSuccStates(dst, b, rays, blackBishop);
             //genPiecesSuccStates(dst, b, jumps, blackKnight);
-            genPiecesSuccStates(dst, b, rays, blackRook);
-            //genPiecesSuccStates(dst, b, rays, blackQueen);
+            //genPiecesSuccStates(dst, b, rays, blackRook);
+            genPiecesSuccStates(dst, b, rays, blackQueen);
             //genPiecesSuccStates(dst, b, rays, blackKing);
         }
 }
@@ -504,8 +503,8 @@ void (*genPieceSuccStates(int piece))(T_boardStates *dst, const T_boardState *b,
             return &genJumpOrStepSuccStates;
         case blackRook:
             return &genRaySuccStates;
-//        case blackQueen:
-//            return &genWBishopSuccStates;
+        case blackQueen:
+            return &genRaySuccStates;
 //        case blackKing:
 //            return &genWBishopSuccStates;
         default:
