144ebb8d5fa594a86a686218101661f81256cdb8
Dejan Popovic <dejan.dp.popovic@gmail.com>
2021-02-14 11:32:24 +0200

    added isSamePosition

--- toAlgNotation.cpp
+++ toAlgNotation.cpp
@@ -71,18 +71,26 @@ char piece(T_boardState *c, char pos){
     }
 }
 
+bool isSamePosition(const char a, const char b){
+    if(a == b){
+        return true;
+    }
+    else
+        return false;
+}
+
 bool arePiecesInSameRank(vector<char> ps, char p){
-//    for(int i = 0; i < ps->freeIndex; i++){
+    for(int i = 0; i < ps.size(); i++){
 ////        if(isSamePosition(ps->positions[i], p)){
 ////            continue;
 ////        }
 //        if(isSameRank(ps->positions[i], p)){
 //            return true;
 //        }
-//    }
+    }
 //    return false;
 }
-//
+
 bool arePiecesInSameFile(vector<char> ps, char p){
 //    for(int i = 0; i < ps->freeIndex; i++){
 ////        if(isSamePosition(ps->positions[i], p)){
