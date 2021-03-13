9ab6f52cf6dbd876230c3c702f04c8193915ee96
Dejan Popovic <dejan.dp.popovic@gmail.com>
2021-03-14 00:57:19 +0200

    implemented createRandomKey

--- keyUtilities.cpp
+++ keyUtilities.cpp
@@ -2,6 +2,17 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-uint64_t rand64(void){
+uint64_t rand64(){
     return rand() ^ ((uint64_t)rand() << 15) ^ ((uint64_t)rand() << 30) ^ ((uint64_t)rand() << 45) ^ ((uint64_t)rand() << 60);
+}
+
+uint64_t ***createRandomKey(){
+    uint64_t ***result = (uint64_t ***)malloc(1 * sizeof(key));
+    for(int i = 0; i < MAX_PIECES; i++){
+        for(int j = 0; j < MAX_COLOURS; j++){
+            for(int k = 0; k < MAX_POSITIONS; k++){
+                result[i][j][k] = rand64();
+            }
+        }
+    }
 }
--- keyUtilities.hpp
+++ keyUtilities.hpp
@@ -1,8 +1,17 @@
 #ifndef KEYUTILITIES_H
 #define KEYUTILITIES_H
 
+#define MAX_PIECES 32
+#define MAX_COLOURS 2
+#define MAX_POSITIONS 64
+
 #include <stdint.h>
 
-uint64_t rand64(void);
+typedef uint64_t key[MAX_PIECES][MAX_COLOURS][MAX_POSITIONS];
+
+uint64_t rand64();
+
+uint64_t ***createRandomKey();
+
 
 #endif // KEYUTILITIES_H
