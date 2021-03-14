a18f52c1d034de1562032e96dd063f99f834a3fe
Dejan Popovic <dejan.dp.popovic@gmail.com>
2021-03-14 14:53:02 +0200

    implement incrementKey() Pt2

--- keyUtilities.hpp
+++ keyUtilities.hpp
@@ -5,7 +5,6 @@
 #include "state.hpp"
 
 #define PIECE 12
-//#define PLAYERS 2
 #define POSITIONS 64
 
 typedef uint64_t key[PIECE][POSITIONS];
@@ -13,6 +12,6 @@ typedef uint64_t key[PIECE][POSITIONS];
 uint64_t rand64();
 key *createRandomKey();
 uint64_t generateKey(T_boardState *b);
-uint64_t incrementKey(uint64_t key, T_boardState *b, T_boardState *ss);
+uint64_t incrementKey(uint64_t k, T_boardState *b, T_boardState *ss, key *randomKey);
 
 #endif // KEYUTILITIES_H
