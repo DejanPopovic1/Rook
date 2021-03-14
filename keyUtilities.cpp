6645f068c0a6970e8ebb00294997cd0499008e5d
Dejan Popovic <dejan.dp.popovic@gmail.com>
2021-03-14 13:27:21 +0200

    fixed createRandomKey()

--- keyUtilities.cpp
+++ keyUtilities.cpp
@@ -10,11 +10,11 @@ uint64_t rand64(){
     return rand() ^ ((uint64_t)rand() << 15) ^ ((uint64_t)rand() << 30) ^ ((uint64_t)rand() << 45) ^ ((uint64_t)rand() << 60);
 }
 
-uint64_t **createRandomKey(){
-    uint64_t **result = (uint64_t **)malloc(1 * sizeof(key));
+key *createRandomKey(){
+    key *result = (key *)malloc(sizeof(key));
     for(int i = 0; i < PIECE; i++){
         for(int j = 0; j < POSITIONS; j++){
-                result[i][j] = rand64();
+            (*result)[i][j] = rand64();
         }
     }
     return result;
@@ -24,9 +24,14 @@ uint64_t generateKey(T_boardState *b, uint64_t constantRandomNumber){
     uint64_t result = 0;
     T_boardState cpy = *b;
     T_bitboard pc;
+    key *rk = createRandomKey();
+    int j;
     for(int i = 1; i < 13; i++){
         while(pc = getPieceFromPieces(pieceBitboard(&cpy, i))){
-            printTBitboard(pc);
+            j = __builtin_ctzll(pc);
+
+            //printTBitboard(pc);
+            //printf("%d\n", j);
         }
     }
     return result;
