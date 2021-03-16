6340c91df901e0a2489eb89325d95ff6db6220c2
Dejan Popovic <dejan.dp.popovic@gmail.com>
2021-03-16 13:46:46 +0200

    added simple Heuristics

--- heuristics.cpp
+++ heuristics.cpp
@@ -1,1 +1,18 @@
 #include "heuristics.hpp"
+#include "state.hpp"
+
+int evaluateWhite(T_boardState *c){
+    return PAWN_VALUE*(__builtin_popcountll(c->wPawn)) + BISHOP_VALUE*(__builtin_popcountll(c->wBishop)) +
+           KNIGHT_VALUE*(__builtin_popcountll(c->wKnight)) + ROOK_VALUE*(__builtin_popcountll(c->wRook)) +
+           QUEEN_VALUE*(__builtin_popcountll(c->wQueen)) + KING_VALUE*(__builtin_popcountll(c->wKing));
+}
+
+int evaluateBlack(T_boardState *c){
+    return PAWN_VALUE*(__builtin_popcountll(c->bPawn)) + BISHOP_VALUE*(__builtin_popcountll(c->bBishop)) +
+           KNIGHT_VALUE*(__builtin_popcountll(c->bKnight)) + ROOK_VALUE*(__builtin_popcountll(c->bRook)) +
+           QUEEN_VALUE*(__builtin_popcountll(c->bQueen)) + KING_VALUE*(__builtin_popcountll(c->bKing));
+}
+
+int boardEvaluation(T_boardState *c){
+    return evaluateWhite(c) - evaluateBlack(c);
+}
--- heuristics.hpp
+++ heuristics.hpp
@@ -1,7 +1,15 @@
 #ifndef HEURISTICS_H
 #define HEURISTICS_H
 
+#define PAWN_VALUE 1
+#define BISHOP_VALUE 3
+#define KNIGHT_VALUE 3
+#define ROOK_VALUE 5
+#define QUEEN_VALUE 9
+#define KING_VALUE 200
 
-
+int evaluateWhite(T_boardState *c);
+int evaluateBlack(T_boardState *c);
+int boardEvaluation(T_boardState *c);
 
 #endif // HEURISTICS_H
