577c4bad5310b83641aa53dfa035db3f10806774
Dejan Popovic <dejan.dp.popovic@gmail.com>
2021-03-16 14:56:04 +0200

    implemented initialiseSearchTree

--- searchTree.cpp
+++ searchTree.cpp
@@ -1,6 +1,16 @@
 #include "searchTree.hpp"
+#include "moveGeneration.hpp"
 
 //void expandNode(T){
 //
 //
 //}
+
+T_searchNode* initialiseSearchTree(T_boardState *rootState){
+    T_searchNode rootNode;
+    rootNode.b = *rootState;
+    for(int i = 0; i < MAX_SUCC_STATES; i++){
+        rootNode.scc[i] = NULL;
+    }
+    return &rootNode;
+}
