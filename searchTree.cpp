db24c28483760bbdc10dee19fbf7c32a0dfe3cdf
Dejan Popovic <dejan.dp.popovic@gmail.com>
2021-03-20 16:10:42 +0200

    tweaked generateTreeNode()

--- searchTree.cpp
+++ searchTree.cpp
@@ -38,12 +38,74 @@ void genSuccStatesSTUB(T_Node *node){
     node->scc[2] = (T_Node*)malloc(sizeof(T_Node));
     node->scc[3] = (T_Node*)malloc(sizeof(T_Node));
     node->scc[4] = (T_Node*)malloc(sizeof(T_Node));
-    node->scc[5] = NULL;
+    node->scc[5] = (T_Node*)malloc(sizeof(T_Node));
+    node->scc[6] = (T_Node*)malloc(sizeof(T_Node));
+    node->scc[7] = (T_Node*)malloc(sizeof(T_Node));
+    node->scc[8] = (T_Node*)malloc(sizeof(T_Node));
+    node->scc[9] = (T_Node*)malloc(sizeof(T_Node));
+    node->scc[10] = (T_Node*)malloc(sizeof(T_Node));
+    node->scc[11] = (T_Node*)malloc(sizeof(T_Node));
+    node->scc[12] = (T_Node*)malloc(sizeof(T_Node));
+    node->scc[13] = (T_Node*)malloc(sizeof(T_Node));
+    node->scc[14] = (T_Node*)malloc(sizeof(T_Node));
+    node->scc[15] = (T_Node*)malloc(sizeof(T_Node));
+    node->scc[16] = (T_Node*)malloc(sizeof(T_Node));
+    node->scc[17] = (T_Node*)malloc(sizeof(T_Node));
+    node->scc[18] = (T_Node*)malloc(sizeof(T_Node));
+    node->scc[19] = (T_Node*)malloc(sizeof(T_Node));
+    node->scc[20] = (T_Node*)malloc(sizeof(T_Node));
+    node->scc[21] = (T_Node*)malloc(sizeof(T_Node));
+    node->scc[22] = (T_Node*)malloc(sizeof(T_Node));
+    node->scc[23] = (T_Node*)malloc(sizeof(T_Node));
+    node->scc[24] = (T_Node*)malloc(sizeof(T_Node));
+    node->scc[25] = (T_Node*)malloc(sizeof(T_Node));
+    node->scc[26] = (T_Node*)malloc(sizeof(T_Node));
+    node->scc[27] = (T_Node*)malloc(sizeof(T_Node));
+    node->scc[28] = (T_Node*)malloc(sizeof(T_Node));
+    node->scc[29] = (T_Node*)malloc(sizeof(T_Node));
+//    node->scc[30] = (T_Node*)malloc(sizeof(T_Node));
+//    node->scc[31] = (T_Node*)malloc(sizeof(T_Node));
+//    node->scc[32] = (T_Node*)malloc(sizeof(T_Node));
+//    node->scc[33] = (T_Node*)malloc(sizeof(T_Node));
+//    node->scc[34] = (T_Node*)malloc(sizeof(T_Node));
+//    node->scc[35] = (T_Node*)malloc(sizeof(T_Node));
+    node->scc[30] = NULL;
     node->scc[0]->b = initialiseBoardState();
     node->scc[1]->b = CMinitialiseBoardState();
     node->scc[2]->b = SMinitialiseBoardState();
     node->scc[3]->b = initialiseBoardState();
     node->scc[4]->b = PPinitialiseBoardState();
+    node->scc[5]->b = initialiseBoardState();
+    node->scc[6]->b = CMinitialiseBoardState();
+    node->scc[7]->b = SMinitialiseBoardState();
+    node->scc[8]->b = initialiseBoardState();
+    node->scc[9]->b = PPinitialiseBoardState();
+    node->scc[10]->b = initialiseBoardState();
+    node->scc[11]->b = CMinitialiseBoardState();
+    node->scc[12]->b = SMinitialiseBoardState();
+    node->scc[13]->b = initialiseBoardState();
+    node->scc[14]->b = PPinitialiseBoardState();
+    node->scc[15]->b = initialiseBoardState();
+    node->scc[16]->b = CMinitialiseBoardState();
+    node->scc[17]->b = SMinitialiseBoardState();
+    node->scc[18]->b = initialiseBoardState();
+    node->scc[19]->b = PPinitialiseBoardState();
+    node->scc[20]->b = initialiseBoardState();
+    node->scc[21]->b = CMinitialiseBoardState();
+    node->scc[22]->b = SMinitialiseBoardState();
+    node->scc[23]->b = initialiseBoardState();
+    node->scc[24]->b = PPinitialiseBoardState();
+    node->scc[25]->b = initialiseBoardState();
+    node->scc[26]->b = CMinitialiseBoardState();
+    node->scc[27]->b = SMinitialiseBoardState();
+    node->scc[28]->b = initialiseBoardState();
+    node->scc[29]->b = PPinitialiseBoardState();
+//    node->scc[30]->b = initialiseBoardState();
+//    node->scc[31]->b = CMinitialiseBoardState();
+//    node->scc[32]->b = SMinitialiseBoardState();
+//    node->scc[33]->b = initialiseBoardState();
+//    node->scc[34]->b = PPinitialiseBoardState();
+//    node->scc[35]->b = initialiseBoardState();
 }
 
 //Optimisation hack: Have two generateLinkedList functions - one for white and one for black. These then recursively call one another. More efficient because:
@@ -58,21 +120,20 @@ int evaluateSTUB(T_Node **iterator){
 
 int generateTreeNode(T_Node **iterator, int level){
     if(level == DEPTH_LIMIT_LEVEL){
-        cout << "Leaf evaluated" << endl;
+        //cout << "Leaf evaluated" << endl;
         return evaluateSTUB(iterator);//Heuristically evaluate the state and return this evaluated value. For now, let it evaluate to
-
     }
     level++;
     genSuccStatesSTUB(*iterator);
     if((*iterator)->b.whosTurn){
-        cout << "black turn" << endl;
+        //cout << "black turn" << endl;
         int min = 1000;
         int e;
-        cout << "This should print 1" << endl;
+        //cout << "This should print 1" << endl;
         //cout << newNode->scc[0]<< endl;
         for(int i = 0; (*iterator)->scc[i] != NULL; i++){
-            cout << "Test black" << endl;
-            e = generateTreeNode(iterator, level);
+            //cout << "Test black" << endl;
+            e = generateTreeNode(&(*iterator)->scc[i], level);
             if(e < min){
                 min = e;
             }
@@ -80,14 +141,14 @@ int generateTreeNode(T_Node **iterator, int level){
         return min;
     }
     else{
-        cout << "white turn" << endl;
+        //cout << "white turn" << endl;
         int max = -1000;
         int e;
-        cout << "This should print 2" << endl;
+        //cout << "This should print 2" << endl;
 //                cout << newNode->scc[0]<< endl;
         for(int i = 0; (*iterator)->scc[i] != NULL; i++){
-            cout << "Test white" << endl;
-            e = generateTreeNode(iterator, level);
+            //cout << "Test white" << endl;
+            e = generateTreeNode(&(*iterator)->scc[i], level);
             if(e > max){
                 max = e;
             }
